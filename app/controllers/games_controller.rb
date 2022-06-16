a8eeec916b084e4ad6c2c19e7b7a762ae64f7c88
Sayonara Silva <sayow@hotmail.com>
2022-06-16 15:58:43 +0100

    continuação - UL inline

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,10 @@
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
 
   def new
-    @letters = ('A'..'Z').to_a.sample(10)
-
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: 'games#home'
   get 'new', to: 'games#new'
   post 'score', to: 'games#score'
 
 
-=======
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> cdf445f4612e8aa87d0729b7def1f4aa5d6145fd
 end
