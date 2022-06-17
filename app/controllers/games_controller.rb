4518189d7dbfcfb3bda19ebf14690f1e8a72c436
Sayonara Silva <sayow@hotmail.com>
2022-06-17 11:11:47 +0100

    dia 3 - erro

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class GamesController < ApplicationController
   VOWELS = %w(A E I O U Y)
 
@@ -8,6 +11,21 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
